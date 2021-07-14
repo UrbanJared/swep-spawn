0fe032fa8c8eabd4298eb3375106c49f563f0e94
UrbanJared <38169333+UrbanJared@users.noreply.github.com>
2021-07-14 14:30:21 +1000

    Added basic menu layout

--- lua/urbanweapon/elements/frame.lua
+++ lua/urbanweapon/elements/frame.lua
@@ -1,8 +1,32 @@
 local PANEL = {}
 
---function PANEL:Init()
---
---end
+function PANEL:Init()
+	local scrollpanel = vgui.Create("DScrollPanel")
+	local weaponBtn = vgui.Create("DButton", self)
+	weaponBtn:SetText("Change Weapon")
+	weaponBtn:Dock(TOP)
+	local posLabel = vgui.Create("DLabel", self)
+	posLabel:SetText("Position")
+	posLabel:Dock(TOP)
+	self.posEntry = vgui.Create("DTextEntry", self)
+	self.posEntry:Dock(TOP)
+	self.posEntry:SetEditable(false)
+	local respawnLabel = vgui.Create("DLabel", self)
+	respawnLabel:SetText("Respawn Time (seconds)")
+	respawnLabel:Dock(TOP)
+	self.respawnEntry = vgui.Create("DTextEntry", self)
+	self.respawnEntry:Dock(TOP)
+
+	local saveBtn = vgui.Create("DButton", self)
+	saveBtn:SetText("Save")
+	saveBtn:SetTall(40)
+	saveBtn:Dock(BOTTOM)
+	local deleteBtn = vgui.Create("DButton", self)
+	deleteBtn:SetText("Delete")
+	deleteBtn:SetTall(40)
+	deleteBtn:DockMargin(0,0,0,5)
+	deleteBtn:Dock(BOTTOM)
+end
 --
 --function PANEL:PerformLayout(w, h)
 --
