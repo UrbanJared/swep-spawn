dbca0fd833d8e4b67fd3fe31389720eb47c42ba7
UrbanJared <38169333+UrbanJared@users.noreply.github.com>
2021-07-14 20:24:48 +1000

    Added Saving to SQL Table

--- lua/autorun/network.lua
+++ lua/autorun/network.lua
@@ -3,10 +3,17 @@ if SERVER then
 	
 	net.Receive("UrbanWeaponSpawnsSetConfig",function()
 		local ent = net.ReadEntity()
-		print("Id: " .. tostring(ent:GetNWInt("id")))
+		local id = ent:GetNWInt("id")
 		local weapon = net.ReadString()
 		local pos = net.ReadString()
 		local respawnTime = net.ReadFloat()
 		ent:SetConfig(id, weapon, pos, respawnTime)
+		if id == 0 then
+			local insertQuery = sql.Query("INSERT INTO urbws_weapons (weapon, map, pos, respawnTime) VALUES ('"..weapon.."','"..game.GetMap().."','"..pos.."','"..respawnTime.."')")
+			local findQuery = sql.Query("SELECT rowid FROM urbws_weapons WHERE weapon = '"..weapon.."' AND pos = '" ..pos.. "'")
+			ent:SetNWInt(id, findQuery.rowid)
+		else
+			local updateQuery = sql.Query("UPDATE urbws_weapons SET weapon = '" ..weapon.."', pos = '" ..pos.."', respawnTime = " ..respawnTime.." WHERE rowid = " .. id)
+		end
 	end)	
 end
