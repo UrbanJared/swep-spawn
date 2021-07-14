d738c8874dfb30ad08ce02a007820c385ebe71bb
UrbanJared <38169333+UrbanJared@users.noreply.github.com>
2021-07-14 10:56:16 +1000

    Minor optimizations

--- lua/entities/urbweaponspawn/init.lua
+++ lua/entities/urbweaponspawn/init.lua
@@ -3,9 +3,9 @@ AddCSLuaFile( "shared.lua" )
 include('shared.lua')
 
 function ENT:Initialize()
-	self.respawnTime = nil
-	self.weapon = "weapon_medkit"
-	self:SetModel( weapons.Get(self.weapon).WorldModel )
+	self.respawnTime = 30
+	self.weapon = weapons.Get("weapon_medkit")
+	self:SetModel( self.weapon.WorldModel )
 	self.Entity:SetUseType( SIMPLE_USE )
 	self:SetSolid( SOLID_VPHYSICS )
 end
@@ -17,22 +17,22 @@ end
 function ENT:SetConfig(weapon, pos, respawnTime)
 	self.respawnTime = respawnTime
 	self:SetPos(Vector(pos))
-	self.weapon = weapon
-	self:SetModel( weapons.Get(weapon).WorldModel )
+	self.weapon = weapons.Get(weapon)
+	self:SetModel( self.weapon.WorldModel )
 end
 
 function ENT:StartTouch(ply)
 	if ply:IsPlayer() then
-		if ply:Give(self.weapon) == NULL then
-			if weapons.Get(self.weapon).Primary.ClipSize > 0 then
-				ply:GiveAmmo(weapons.Get(self.weapon).Primary.ClipSize, weapons.Get(self.weapon).Primary.Ammo)
+		if ply:Give(self.weapon.ClassName) == NULL then
+			if self.weapon.Primary.ClipSize > 0 then
+				ply:GiveAmmo(self.weapon.Primary.ClipSize, self.weapon.Primary.Ammo)
 			else
-				ply:GiveAmmo(weapons.Get(self.weapon).Primary.DefaultClip, weapons.Get(self.weapon).Primary.Ammo)
+				ply:GiveAmmo(self.weapon.Primary.DefaultClip, self.weapon.Primary.Ammo)
 			end
 		end
 		self:SetNoDraw(true)
 		self:SetSolid( SOLID_NONE )
-		timer.Simple(self.respawnTime or 30, function()
+		timer.Simple(self.respawnTime, function()
 			self:SetSolid( SOLID_VPHYSICS )
 			self:SetNoDraw(false)
 		end)
